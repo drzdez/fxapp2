e52cbb5c2bb9f212c6db021ccb619721d8831114
zzd <zdrazilz@seznam.cz>
2022-05-06 14:47:32 +0200

    packing

--- app/src/main/java/fxapp2/xmldoc/XMLWrapper.java
+++ app/src/main/java/fxapp2/xmldoc/XMLWrapper.java
@@ -1,7 +1,5 @@
 package fxapp2.xmldoc;
 
-import java.io.File;
-
 
 /**
   Interface for XML parsers used in app to be able to switch to other implementation like DOM4J 
@@ -9,15 +7,6 @@ import java.io.File;
 public interface XMLWrapper {
 
     public void readNode();
-
-
-  
-
-/*
-  DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-  DocumentBuilder db = dbf.newDocumentBuilder(); 
-  Document doc = db.parse(new File(filename));
-*/
 
 
 
